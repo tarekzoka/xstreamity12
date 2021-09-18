f6c71cc9e45e5a2f07df747d5805714e5291475a
tarekhanfy <65197356+tarekzoka@users.noreply.github.com>
2021-09-18 21:36:35 +0200

    Update installer.sh

--- installer.sh
+++ installer.sh
@@ -7,6 +7,10 @@
 wait
 opkg install --force-overwrite  https://github.com/tarekzoka/-xstreamity/raw/main/enigma2-plugin-extensions-xstreamity_3.18.20210917_all.ipk
 wait
+wget -O /tmp/xstreamity_3.18.20210917_all.ipk "https://raw.githubusercontent.com/tarekzoka/-xstreamity/main/xstreamity_3.18.20210917_all.ipk"
+wait
+opkg install --force-overwrite /tmp/*.ipk
+wait
 sleep 2;
 exit 0
 
