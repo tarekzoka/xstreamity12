b450f7383265cc324814e65337da76cb0eef9178
tarekhanfy <65197356+tarekzoka@users.noreply.github.com>
2021-09-18 16:55:45 +0200

    Update installer.sh

--- installer.sh
+++ installer.sh
@@ -7,26 +7,14 @@
 #command: wget https://raw.githubusercontent.com/tarekzoka/-xstreamity/main/installer.sh -O - | /bin/sh
  #
 MY_EM="*********************************************************************************"
-wget -O /tmp/xstreamity_3.16_all.ipk "https://raw.githubusercontent.com/tarekzoka/-xstreamity/main/xstreamity_3.16_all.ipk"
+#!/bin/sh
+#
+
+wget -O /tmp/xstreamity_3.16_all.ipk "ttps://github.com/tarekzoka/-xstreamity/blob/main/enigma2-plugin-extensions-xstreamity_3.16_all.ipk"
 wait
-opkg install /tmp/*.ipk
-echo $MY_EM
-######      INSTALL PLUGIN xstreamit ###
-opkg install --force-overwrite /tmp/enigma2-plugin-extensions-xstreamity_3.16_all.ipk
+opkg install --force-overwrite /tmp/*.ipk
 wait
-echo $MY_EM
-#######   REMOVE PACKAGE FROM TMP ###
-rm -r /tmp/xstreamity_3.16_all.ipk 
-echo "========================================================================" 	
-echo ". WELCOME TO PLUGIN XCPLUGIN "
-echo $MY_EM
-	echo " PLUGIN INSTALLED SUCCESSFULLY "
-echo " UPLOADED BY TARE_TAREKL " 
-echo "======================================================"
-echo "       YOUR DEVICE WELL RESTART Now " 
-echo "****************************************"
-wait 3;
-init 4
-init 3
+rm -r /tmp/ncam-revcam_all.ipk
+wait
+sleep 2;
 exit 0
-https://github.com/tarekzoka/-xstreamity/blob/main/enigma2-plugin-extensions-xstreamity_3.16_all.ipk
