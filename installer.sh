ae65514faf2fb282d53abe911a6d7dc7f5443efa
tarekhanfy <65197356+tarekzoka@users.noreply.github.com>
2021-09-18 16:01:25 +0200

    Update installer.sh

--- installer.sh
+++ installer.sh
@@ -4,10 +4,10 @@
 # SCRIPT : DOWNLOAD AND INSTALL XCPLUGIN
 #
 ###########################################
-#command: wget https://raw.githubusercontent.com/tarekzoka/xstreamity/main/installer.sh -O - | /bin/sh
+#command: wget https://raw.githubusercontent.com/tarekzoka/-xstreamity/main/installer.sh -O - | /bin/sh
  #
 MY_EM="*********************************************************************************"
-wget -O /tmp/xstreamity_3.16_all.ipk "https://raw.githubusercontent.com/tarekzoka/xstreamity/main/xstreamity_3.16_all.ipk"
+wget -O /tmp/xstreamity_3.16_all.ipk "https://raw.githubusercontent.com/tarekzoka/xstreamity/main/-xstreamity_3.16_all.ipk"
 wait
 echo $MY_EM
 ######      INSTALL PLUGIN xstreamit ###
