de1ad611c19a4eea52673b840f00b2137e4ae607
tarekhanfy <65197356+tarekzoka@users.noreply.github.com>
2021-09-18 22:20:15 +0200

    Update installer.sh

--- installer.sh
+++ installer.sh
@@ -1,20 +1,31 @@
 #!/bin/sh
 #
-# SCRIPT : DOWNLOAD AND INSTALL -xstreamity
+# SCRIPT : DOWNLOAD AND INSTALL XCPLUGIN
 #
 ###########################################
 #command: wget https://raw.githubusercontent.com/tarekzoka/-xstreamity/main/installer.sh -O - | /bin/sh
+ #
+MY_EM="*********************************************************************************"
+wget -O /var/volatile/tmp/xstreamity_3.18.20210917_all.ip "https://github.com/tarekzoka/-xstreamity/raw/main/enigma2-plugin-extensions-xstreamity_3.18.20210917_all.ipk"
 wait
-opkg install --force-overwrite  https://github.com/tarekzoka/-xstreamity/raw/main/enigma2-plugin-extensions-xstreamity_3.18.20210917_all.ipk
-wait
-wget -O /tmp/xstreamity_3.18.20210917_all.ipk "https://raw.githubusercontent.com/tarekzoka/-xstreamity/main/xstreamity_3.18.20210917_all.ipk"
-wait
+echo $MY_EM
+######      INSTALL PLUGIN xstreamity ###
 opkg install --force-overwrite /tmp/*.ipk
 wait
-sleep 2;
+echo $MY_EM
+#######   REMOVE PACKAGE FROM TMP ###
+rm -r /var/volatile/tmp/xstreamity_3.18.20210917_all.ipk 
+echo "========================================================================" 	
+echo ". WELCOME TO PLUGIN XCPLUGIN "
+echo $MY_EM
+	echo " PLUGIN INSTALLED SUCCESSFULLY "
+echo " UPLOADED BY EMIL_NABIL " 
+echo "======================================================"
+echo "       YOUR DEVICE WELL RESTART Now " 
+echo "****************************************"
+wait 3;
+init 4
+init 3
 exit 0
-
-
-
 
 
