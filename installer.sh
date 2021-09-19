5057fd48cb9507fc1a1f0c4bb802de590de5891e
tarekhanfy <65197356+tarekzoka@users.noreply.github.com>
2021-09-20 00:33:40 +0200

    Update installer.sh

--- installer.sh
+++ installer.sh
@@ -7,17 +7,17 @@
 #command: wget https://raw.githubusercontent.com/tarekzoka/-xstreamity/main/installer.sh -O - | /bin/sh
  #
 MY_EM="*********************************************************************************"
-wget -O /tmp/xstreamity_1.8_all.ipk "https://raw.githubusercontent.com/tarekzoka/iptosat/main/enigma2-plugin-extensions-xstreamity_3.18.20210917_all.ipk"
+wget -O /tmp/xstreamity_3.18.20210917_all.ipk "https://raw.githubusercontent.com/tarekzoka/iptosat/main/enigma2-plugin-extensions-xstreamity_3.18.20210917_all.ipk"
 wait
 echo $MY_EM
-######      INSTALL PLUGIN iptosat ###
+######      INSTALL PLUGIN xstreamity ###
 opkg install --force-overwrite /tmp/*.ipk
 wait
 echo $MY_EM
 #######   REMOVE PACKAGE FROM TMP ###
 rm -r /tmp/xstreamity_1.8_all.ipk 
 echo "========================================================================" 	
-echo ". WELCOME TO PLUGIN XCPLUGIN "
+echo ". WELCOME TO PLUGIN xstreamity "
 echo $MY_EM
 	echo " PLUGIN INSTALLED SUCCESSFULLY "
 echo " UPLOADED BY TARE_TAREKL " 
