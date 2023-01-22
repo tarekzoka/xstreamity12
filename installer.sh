8c705db6126ceb653d4e1145f05ecd608c61cb80
tarekhanfy <65197356+tarekzoka@users.noreply.github.com>
2023-01-22 22:46:01 +0200

    Add files via upload

--- installer.sh
+++ installer.sh
@@ -8,8 +8,6 @@
 echo
 
 opkg install --force-overwrite  https://github.com/tarekzoka/xstreamity12/raw/main/enigma2-plugin-extensions-xstreamity_3.92_all.ipk
-echo ". >>>>         RESTARING     <<<<"
-echo "**********************************************************************************"
 wait
 killall -9 enigma2
 exit 0     ,
