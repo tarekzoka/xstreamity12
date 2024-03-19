571d788c13df5731937e3630f4a1b3c34440cc76
tarekhanfy <65197356+tarekzoka@users.noreply.github.com>
2024-03-20 01:15:31 +0200

    Add files via upload

--- installer-os.sh
+++ installer-os.sh
@@ -1,15 +1,15 @@
-#!/bin/bash
-######################################################################################
-## Command=wget https://raw.githubusercontent.com/tarekzoka/xstreamity12/main/installer-os.sh -O - | /bin/sh
-###########################################
-###########################################
 #!/bin/sh
 #
-wget -O /tmp/enigma2-plugin-extensions-xstreamity_4.40.20240309_all.deb "https://github.com/tarekzoka/xstreamity12/blob/main/enigma2-plugin-extensions-xstreamity_4.40.20240309_all.deb?raw=true"
+wget -O /tmp/xstreamity_4.40.20240309_all.deb "https://github.com/tarekzoka/xstreamity12/blob/main/enigma2-plugin-extensions-xstreamity_4.40.20240309_all.deb?raw=true"
+
 wait
-apt-get update
-dpkg -i /tmp/*.deb
-apt-get -f -y installl
-sleep 2;
+
+apt-get update ; dpkg -i /tmp/*.deb 
+
+wait
+
+killall -9 enigma2
+
 exit 0
+
 
