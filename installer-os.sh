35585111edbfc84a1b6340a7d8a0108820f5ae4b
tarekhanfy <65197356+tarekzoka@users.noreply.github.com>
2024-03-20 01:28:21 +0200

    Add files via upload

--- installer-os.sh
+++ installer-os.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 #
-wget -O /tmp/xstreamity_4.40.20240309_all.deb "https://github.com/tarekzoka/xstreamity12/blob/main/enigma2-plugin-extensions-xstreamity_4.40.20240309_all.deb?raw=true"
+wget -O /tmp/xstreamity_4.40.20240309_all.deb "https://github.com/tarekzoka/xstreamity12/blob/main/enigma2-plugin-extensions-xstreamity_4.40.20240309_all.deb"
 
 wait
 
